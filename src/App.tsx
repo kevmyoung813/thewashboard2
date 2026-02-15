0d8c5e86d080fde71aaae7e791f992981f2ea475
kevmyoung813 <sidekyck.services@gmail.com>
2026-02-15 13:13:18 -0600

    Phase 1: Remove unused UI components and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
@@ -14,30 +12,24 @@ import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/how-to" element={<HowTo />} />
-          <Route path="/how-to/washers" element={<HowToWashers />} />
-          <Route path="/how-to/dryers" element={<HowToDryers />} />
-          <Route path="/how-to/laundry-card" element={<HowToLaundryCard />} />
-          <Route path="/rules" element={<Rules />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Sonner />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/how-to" element={<HowTo />} />
+        <Route path="/how-to/washers" element={<HowToWashers />} />
+        <Route path="/how-to/dryers" element={<HowToDryers />} />
+        <Route path="/how-to/laundry-card" element={<HowToLaundryCard />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export default App;
